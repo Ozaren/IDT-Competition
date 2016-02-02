b0630a68653dbdbf15b43d7cd2a6905ff86f6934
Jarett Lee <googleplex_unknown@yahoo.com>
2016-02-02 14:39:27 -0500

    format add package

--- PackageTrackerGoL/src/gameOfLife/packageTracker/util/ui/guiComputer/overview/OverviewTracker.java
+++ PackageTrackerGoL/src/gameOfLife/packageTracker/util/ui/guiComputer/overview/OverviewTracker.java
@@ -11,50 +11,64 @@ import gameOfLife.packageTracker.util.ui.guiComputer.PackageTrackerFrame;
 @SuppressWarnings("serial")
 public class OverviewTracker extends JPanel
 {
-	   public OverviewTracker()
-	   {
-	      Dimension dimension = new Dimension(600, 150);
-	      setPreferredSize(dimension);
-	      setMaximumSize(dimension);
-	      
-	      setLayout(new GridBagLayout());
-	      
-	      setBorder(BorderFactory.createCompoundBorder(
-					BorderFactory.createEmptyBorder(0, 0, 6, 0),
-					BorderFactory.createLineBorder(Color.black)));
-	      
-	      GridBagConstraints gbc;
-	      
-	      gbc = creategbc(3, 0);
-	      JButton buttonLogOut = new JButton("Log Out");
-	      
-	      buttonLogOut.addActionListener(new ActionListener()
-	      {
-	         
-	         @Override
-	         public void actionPerformed(ActionEvent arg0)
-	         {
-	        	 ((PackageTrackerFrame) getRootPane().getParent()).showLogin();
-	         }
-	      });
-	      
-	      add(buttonLogOut, gbc);
-	      
-	      gbc = creategbc(0, 3);
-	      add(new JLabel("get Package: "), gbc);
-
-	      gbc = creategbc(1, 3);
-	      add(new JTextField("uuid"), gbc);
-	   }
-	   
-	   private static GridBagConstraints creategbc(int x, int y)
-	   {
-		   GridBagConstraints gbc = new GridBagConstraints();
-		   gbc.gridx = x;
-		   gbc.gridy = y;
-		   
-		   return gbc;
-	   }
-	   
-	   
+	
+	
+	public OverviewTracker()
+	{
+		Dimension dimension = new Dimension(600, 150);
+		setPreferredSize(dimension);
+		setMaximumSize(dimension);
+		
+		setLayout(new GridBagLayout());
+		
+		setBorder(BorderFactory.createCompoundBorder(
+						BorderFactory.createEmptyBorder(0, 0, 6, 0),
+						BorderFactory.createLineBorder(Color.black)));
+		
+		GridBagConstraints gbc;
+		
+		
+		JButton buttonLogOut = new JButton("Log Out");
+		
+		buttonLogOut.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0)
+			{
+			  	 ((PackageTrackerFrame) getRootPane().getParent()).showLogin();
+			}
+		});
+		
+		gbc = creategbc(2, 0);
+		add(buttonLogOut, gbc);
+		
+		gbc = creategbc(0, 1);
+		add(new JLabel("Get Package: "), gbc);
+		
+		gbc = creategbc(1, 1);
+		add(new JTextField("uuid"), gbc);
+		
+		JButton buttonGetPackage = new JButton("Lookup");
+		
+//		buttonGetPackage.addActionListener(new ActionListener()
+//		{
+//			@Override
+//			public void actionPerformed(ActionEvent arg0)
+//			{
+//			  	 buttonGetPackage.super.actionPerformed();
+//			}
+//		});
+		gbc = creategbc(2, 1);
+		add(buttonGetPackage, gbc);
+		
+	}
+	
+	private static GridBagConstraints creategbc(int x, int y)
+	{
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = x;
+		gbc.gridy = y;
+		
+		return gbc;
+	}
 }
